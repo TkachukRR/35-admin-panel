f8da61033bc19717d77a0ccbde2ff36b9e53592d
Roman Tkachuk <tkachukrr@gmail.com>
2023-09-26 23:58:37 +0200

    Added user service

---
+++ src/app/services/users.service.ts
@@ -0,0 +1,17 @@
+import { inject, Injectable, signal } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { toSignal } from "@angular/core/rxjs-interop";
+import { User } from "../interfaces/user";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsersService {
+  private localFile = 'assets/users.json';
+
+  private _http = inject(HttpClient);
+
+  private _users$ = this._http.get<User[]>(this.localFile);
+
+  public users = toSignal(this._users$, {initialValue : [] as User[]})
+}
