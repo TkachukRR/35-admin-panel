8fd271f521a0cc61684f065ebf64fc13cf58a5c0
Roman Tkachuk <tkachukrr@gmail.com>
2023-09-27 16:23:39 +0200

    Implemented ability show create-edit form

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
+import { UsersService } from "../services/users.service";
 
 @Component({
   selector: 'app-main-page',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./main-page.component.scss']
 })
 export class MainPageComponent {
+  private _userService = inject(UsersService);
+  private selectedUser = this._userService.selectedUserName;
 
+  private editUserFormVisible = signal(!!this.selectedUser())
+  private createUserFormVisible = signal(false)
+
+  public formVisible = computed( () => this.editUserFormVisible() || this.selectedUser() || this.createUserFormVisible() )
+
+  public onCreateUser() {
+    this.selectedUser.set('')
+    this.editUserFormVisible.set(false)
+    this.createUserFormVisible.set(true)
+  }
 }
