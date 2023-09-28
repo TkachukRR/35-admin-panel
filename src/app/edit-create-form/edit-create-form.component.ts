3605995759a5a05d82d753e97d232fb056530158
Roman Tkachuk <tkachukrr@gmail.com>
2023-09-28 14:08:06 +0200

    Added form fields validators

--- src/app/edit-create-form/edit-create-form.component.ts
+++ src/app/edit-create-form/edit-create-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, inject, Input, OnChanges, OnInit, signal } from '@angular/core';
-import { FormBuilder, FormGroup } from "@angular/forms";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { FormState } from "../enums/form-state";
 
 @Component({
@@ -16,15 +16,15 @@ export class EditCreateFormComponent implements OnInit, OnChanges{
   public formState = signal<FormState>(FormState.create);
 
   ngOnInit(): void {
-      this.userForm = this._fb.group({
-        username: [''],
-        firstName: [''],
-        lastName: [''],
-        email: [''],
-        type: [''],
-        password: [''],
-        repeatPassword: [''],
-      })
+    this.userForm = this._fb.group({
+      username: ['', [Validators.required, Validators.minLength(8), Validators.pattern(/^[a-zA-Z0-9]+$/)]],
+      firstName: ['', [Validators.required, Validators.pattern(/^[a-zA-Zа-яА-Я]+$/)]],
+      lastName: ['', [Validators.required, Validators.pattern(/^[a-zA-Zа-яА-Я]+$/)]],
+      email: ['', [Validators.required, Validators.email]],
+      type: ['', Validators.required],
+      password: ['', [Validators.required, Validators.pattern(/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@#$!%*?&])[A-Za-z\d@#$!%*?&]+$/)]],
+      repeatPassword: ['', [Validators.required]],
+    })
   }
 
   ngOnChanges(){
