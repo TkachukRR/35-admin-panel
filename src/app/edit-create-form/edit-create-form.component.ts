844da7ceec05f4367540ce1feb7f7ee5a517f716
Roman Tkachuk <tkachukrr@gmail.com>
2023-10-05 00:57:36 +0200

    Changed form logic and states

--- src/app/edit-create-form/edit-create-form.component.ts
+++ src/app/edit-create-form/edit-create-form.component.ts
@@ -1,45 +1,63 @@
-import { Component, inject, Input, OnChanges, OnInit, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { FormState } from "../enums/form-state";
 import { UserType } from "../enums/user-type";
+import { UsersService } from "../services/users.service";
+import { toObservable } from "@angular/core/rxjs-interop";
 
 @Component({
   selector: 'app-edit-create-form',
   templateUrl: './edit-create-form.component.html',
   styleUrls: ['./edit-create-form.component.scss']
 })
-export class EditCreateFormComponent implements OnInit, OnChanges{
+export class EditCreateFormComponent{
   private _fb = inject(FormBuilder);
+  private _us = inject(UsersService)
 
-  @Input() userName = '';
+  private userSelected = this._us.userSelected
+  private selectedName = this._us.selectedName
+
+  public formState = signal<FormState>(FormState.create);
+  public loading = this._us.loadingUserInfo
+  public editFormTitle = signal('Edit user')
 
   public userTypes: UserType[] = [ UserType.Administrator, UserType.Driver]
-  public userForm!: FormGroup;
-  public formState = signal<FormState>(FormState.create);
+  public userForm: FormGroup = this._fb.group({
+    username: ['', [Validators.required, Validators.minLength(8), Validators.pattern(/^[a-zA-Z0-9]+$/)]],
+    firstName: ['', [Validators.required, Validators.pattern(/^[a-zA-Zа-яА-Я]+$/)]],
+    lastName: ['', [Validators.required, Validators.pattern(/^[a-zA-Zа-яА-Я]+$/)]],
+    email: ['', [Validators.required, Validators.email]],
+    type: ['', Validators.required],
+    password: ['', [Validators.required, Validators.pattern(/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@#$!%*?&])[A-Za-z\d@#$!%*?&]+$/)]],
+    repeatPassword: ['', Validators.required]
+  })
 
-  ngOnInit(): void {
-    this.userForm = this._fb.group({
-      username: ['', [Validators.required, Validators.minLength(8), Validators.pattern(/^[a-zA-Z0-9]+$/)]],
-      firstName: ['', [Validators.required, Validators.pattern(/^[a-zA-Zа-яА-Я]+$/)]],
-      lastName: ['', [Validators.required, Validators.pattern(/^[a-zA-Zа-яА-Я]+$/)]],
-      email: ['', [Validators.required, Validators.email]],
-      type: ['', Validators.required],
-      password: ['', [Validators.required, Validators.pattern(/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@#$!%*?&])[A-Za-z\d@#$!%*?&]+$/)]],
-      repeatPassword: ['', Validators.required]
+  selectedUser$ = toObservable((this.userSelected)).subscribe(
+    (user) => {
+      this.editFormTitle.set(`${user.first_name} ${user.last_name}`)
+      this.userForm.patchValue({
+        username: user.username,
+        firstName: user.first_name,
+        lastName: user.last_name,
+        email: user.email,
+        type: user.user_type,
+        password: user.password,
+        repeatPassword: user.password
+      })
     })
-  }
 
-  ngOnChanges(){
-    switch (true) {
-      case !!this.userName:
-        this.formState.set(FormState.edit);
-        break;
-
-      default:
-        this.formState.set(FormState.create);
-        break;
+  loading$ = toObservable(this.loading).subscribe(
+    () => {
+      this.editFormTitle.set('Edit user')
     }
-  }
+  )
+
+  selectedName$ = toObservable(this.selectedName).subscribe(
+    name => {
+      if (name === '') this.formState.set(FormState.create);
+      else this.formState.set(FormState.edit);
+    }
+  )
 
   public onSubmit(){
   }
