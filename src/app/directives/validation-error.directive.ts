bbde1c245b4811d7581d1119efa99f93da216d05
Roman Tkachuk <tkachukrr@gmail.com>
2023-10-16 22:36:10 +0200

    Added logic to ValidationErrorDirective

--- src/app/directives/validation-error.directive.ts
+++ src/app/directives/validation-error.directive.ts
@@ -1,16 +1,100 @@
-import { Directive, inject, Input, OnInit, TemplateRef, ViewContainerRef } from '@angular/core';
-import { AbstractControl } from "@angular/forms";
+import { Directive, ElementRef, HostListener, inject, OnDestroy } from '@angular/core';
+import { NgControl } from "@angular/forms";
+import { Subscription } from "rxjs";
 
 @Directive({
   selector: '[appValidationError]'
 })
-export class ValidationErrorDirective implements OnInit{
-  @Input('appValidationError') public userFormControl!: AbstractControl;
+export class ValidationErrorDirective implements OnDestroy{
+  private elRef = inject(ElementRef);
+  private control = inject(NgControl);
 
-  private _viewContainerRef = inject(ViewContainerRef);
-  private _templateRef = inject(TemplateRef);
+  private sub!: Subscription | undefined;
 
-  public ngOnInit(): void {
-    this._viewContainerRef.createEmbeddedView(this._templateRef);
+  private parentWrapperDivEl: HTMLDivElement = this.elRef.nativeElement.parentNode;
+  private inputEl: HTMLInputElement = this.elRef.nativeElement;
+
+  private inputErrors: any = {};
+  private errorMessage = '';
+  private errorMsgDiv!: HTMLDivElement | null;
+
+  @HostListener('focus') onFocus(){
+    this.inputEl.style.outlineColor = "1px solid var(--txt-primary)"
+
+    this.sub = this.control.statusChanges?.subscribe(
+      () => {
+        if (!this.control.errors) {
+          this.inputErrors = {}
+          this.errorMessage = ''
+          if (this.errorMsgDiv) {
+            this.parentWrapperDivEl.removeChild(this.errorMsgDiv)
+            this.errorMsgDiv = null
+          }
+          return
+        }
+
+        Object.keys(this.control.errors).map( errorKey => {
+          this.deleteUnExistingErrors()
+          this.updateErrors(errorKey)
+          this.setErrorMessage()
+          if (!this.errorMsgDiv) {
+            this.prepareErrorDiv()
+            if (this.errorMsgDiv) this.parentWrapperDivEl.append(this.errorMsgDiv)
+          } else {
+            this.errorMsgDiv.textContent = this.errorMessage
+          }
+        })
+      }
+    )
+  }
+
+  @HostListener('blur') onBlur(){
+    if (!this.control.errors) return
+
+    this.inputEl.style.outline = '1px solid var(--bg-msg-error)';
+
+    Object.keys(this.control.errors).map( errorKey => {
+      this.deleteUnExistingErrors()
+      this.updateErrors(errorKey)
+      this.setErrorMessage()
+      if (!this.errorMsgDiv) {
+        this.prepareErrorDiv()
+        if (this.errorMsgDiv) this.parentWrapperDivEl.append(this.errorMsgDiv)
+      } else {
+        this.errorMsgDiv.textContent = this.errorMessage
+      }
+    })
+    this.sub?.unsubscribe()
+  }
+
+  private deleteUnExistingErrors(){
+    Object.keys(this.inputErrors).filter(key => !this.control.errors?.[key]).map(key => delete this.inputErrors[key])
+  }
+
+  private updateErrors(errorKey: string){
+    if (this.control.errors && errorKey === 'required') {
+      this.inputErrors[errorKey] = `required field`
+    }
+    if (this.control.errors && errorKey === 'pattern') {
+      this.inputErrors[errorKey] = `only ${this.control.errors[errorKey]['requiredPattern'].slice(3, -4)}`
+    }
+    if (this.control.errors && errorKey === 'minlength') {
+      this.inputErrors[errorKey] = `minlength ${this.control.errors['minlength']['requiredLength']}`
+    }
+  }
+
+  private setErrorMessage() {
+    const errorKeys = Object.keys(this.inputErrors)
+    this.errorMessage ='Error: ' + errorKeys.map(error => this.inputErrors[error]).join(', ')
+  }
+
+  private prepareErrorDiv(){
+    this.errorMsgDiv = document.createElement('div');
+    this.errorMsgDiv.style.color = 'var(--bg-msg-error)';
+    this.errorMsgDiv.textContent = this.errorMessage
+  }
+
+  public ngOnDestroy(): void {
+    this.sub?.unsubscribe()
   }
 }
